97736e949275e3d7df6cc10691662922b2aa9ed6
Cristian M <cristianmicicoi@gmail.com>
2022-10-25 15:57:53 +0300

    costumizing form input and button

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 // Selectors
 
-const countDate = new Date('Oct 25, 2022 15:00:00').getTime();
+const countDate = new Date("Oct 25, 2022 15:00:00").getTime();
 // const countDate = new Date('Dec 25, 2022 00:01:00').getTime();
 
-const countTime = document.getElementsByClassName('files');
-const output = document.querySelector('.files');
-const submitBtn = document.querySelector('#submit');
-const inputValue = document.querySelector('#datetime');
-let daysElem = document.querySelector('.d');
-let hoursElem = document.querySelector('.h');
-let minElem = document.querySelector('.m');
-let secElem = document.querySelector('.s');
+const countTime = document.getElementsByClassName("files");
+const output = document.querySelector(".files");
+const submitBtn = document.querySelector("#submit");
+const inputValue = document.querySelector("#datetime");
+let daysElem = document.querySelector(".d");
+let hoursElem = document.querySelector(".h");
+let minElem = document.querySelector(".m");
+let secElem = document.querySelector(".s");
 
 // Events
 
-submitBtn.addEventListener('click', function (e) {
+submitBtn.addEventListener("click", function (e) {
   const countDate = new Date(inputValue.value).getTime();
   let x = setInterval(function () {
     let now = new Date().getTime();
     let distance = countDate - now;
     if (distance < 1) {
       clearInterval(x);
-      return (document.getElementById('display').innerHTML = 'EXPIRED!');
+      return (document.getElementById("display").innerHTML = "EXPIRED!");
     }
     // time calculations for days, hours, minutes and seconds
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
